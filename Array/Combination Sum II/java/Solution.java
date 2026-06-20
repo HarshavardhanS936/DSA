6dfe57debbf170393fc8f5fea733e97871cf0c8a
HarshavardhanS936 <shv9306@gmail.com>
2026-06-20 21:03:31 +0530

    feat: Add Combination Sum II

--- Array/Combination Sum II/java/Solution.java
+++ Array/Combination Sum II/java/Solution.java
@@ -1,18 +1,19 @@
 class Solution {
     public List<List<Integer>> combinationSum2(int[] nums, int target) {
-        // List<List<Integer>> ans=new ArrayList<>();
+        List<List<Integer>> ans=new ArrayList<>();
         Arrays.sort(nums);
-        Set<List<Integer>> ans=new HashSet<>();
         sub(0,nums,target,ans,new ArrayList<>());
-        return new ArrayList(ans);
+        return ans;
     }
-    public static void sub(int index,int nums[],int target,Set<List<Integer>> ans,List<Integer> temp){
+    public static void sub(int index,int nums[],int target,List<List<Integer>> ans,List<Integer> temp){
         if(target==0){
             ans.add(new ArrayList<>(temp));
             return;
         }
         if(target<0)return;
         for(int i=index;i<nums.length;i++){
+            if(i>index&&nums[i]==nums[i-1])continue;
+            if(nums[i]>target) break;
             temp.add(nums[i]);
             sub(i+1,nums,target-nums[i],ans,temp);
             temp.remove(temp.size()-1);
