5e9397dfb27b8280c39a3bc89160c7f7e906bdc2
HarshavardhanS936 <shv9306@gmail.com>
2026-06-21 13:38:21 +0530

    feat: Add Add Two Numbers

---
+++ General/Add Two Numbers/java/Solution.java
@@ -0,0 +1,32 @@
+1/**
+2 * Definition for singly-linked list.
+3 * public class ListNode {
+4 *     int val;
+5 *     ListNode next;
+6 *     ListNode() {}
+7 *     ListNode(int val) { this.val = val; }
+8 *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+9 * }
+10 */
+11class Solution {
+12    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+13        ListNode temp=new ListNode(0);
+14        ListNode car=temp;
+15        int carry=0;
+16        while(l1!=null || l2!=null || carry!=0){
+17            int sum=carry;
+18            if(l1!=null) {
+19                sum+=l1.val;
+20                l1=l1.next;
+21            }
+22            if(l2!=null) {
+23                sum+=l2.val;
+24                l2=l2.next;
+25            }
+26            carry=sum/10;
+27            car.next=new ListNode(sum%10);
+28            car=car.next;
+29        }
+30        return temp.next;
+31    }
+32}
