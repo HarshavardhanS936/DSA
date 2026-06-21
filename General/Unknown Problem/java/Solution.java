e94e60e389dabb1e8ef738b9dbf378a4863ca632
HarshavardhanS936 <shv9306@gmail.com>
2026-06-21 17:43:23 +0530

    feat: Add Unknown Problem

--- General/Unknown Problem/java/Solution.java
+++ General/Unknown Problem/java/Solution.java
@@ -1,23 +1,23 @@
-1class Solution {
-2    public List<List<Integer>> permute(int[] nums) {
-3        List<List<Integer>> list=new ArrayList<>();
-4        boolean visit[]=new boolean[nums.length];
-5        per(nums,visit,list,new ArrayList<>());
-6        return list;
-7    }
-8    public void per(int nums[],boolean visit[],List<List<Integer>> list,List<Integer> temp){
-9        if(temp.size()==nums.length){
-10            list.add(new ArrayList<>(temp));
-11            return;
-12        }
-13        for(int i=0;i<nums.length;i++){
-14            if(!visit[i]){
-15                visit[i]=true;
-16                temp.add(nums[i]);
-17                per(nums,visit,list,temp);
-18                temp.remove(temp.size()-1);
-19                visit[i]=false;
-20            }
-21        }
+1/**
+2 * Definition for singly-linked list.
+3 * public class ListNode {
+4 *     int val;
+5 *     ListNode next;
+6 *     ListNode(int x) {
+7 *         val = x;
+8 *         next = null;
+9 *     }
+10 * }
+11 */
+12public class Solution {
+13    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+14        ListNode a=headA,b=headB;
+15        while(a != b){
+16            if(a==null) a=headB;
+17            else a=a.next;
+18            if(b==null)b=headA;
+19            else b=b.next;
+20        }
+21        return a;
 22    }
 23}
