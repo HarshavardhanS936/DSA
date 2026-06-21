009019c83a6ee6b4df2f5bcdf6cf7120d5c77839
HarshavardhanS936 <shv9306@gmail.com>
2026-06-21 13:38:19 +0530

    feat: Add Unknown Problem

--- General/Unknown Problem/java/Solution.java
+++ General/Unknown Problem/java/Solution.java
@@ -1,20 +1,23 @@
-1/**
-2 * Definition for singly-linked list.
-3 * public class ListNode {
-4 *     int val;
-5 *     ListNode next;
-6 *     ListNode() {}
-7 *     ListNode(int val) { this.val = val; }
-8 *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-9 * }
-10 */
-11class Solution {
-12    public ListNode deleteDuplicates(ListNode head) {
-13        ListNode res=head;
-14        while (head!=null && head.next!=null){
-15            if(head.val==head.next.val)head.next=head.next.next;
-16            else head=head.next;
-17        }
-18        return res;
-19    }
-20}
+1class Solution {
+2    public List<List<Integer>> permute(int[] nums) {
+3        List<List<Integer>> list=new ArrayList<>();
+4        boolean visit[]=new boolean[nums.length];
+5        per(nums,visit,list,new ArrayList<>());
+6        return list;
+7    }
+8    public void per(int nums[],boolean visit[],List<List<Integer>> list,List<Integer> temp){
+9        if(temp.size()==nums.length){
+10            list.add(new ArrayList<>(temp));
+11            return;
+12        }
+13        for(int i=0;i<nums.length;i++){
+14            if(!visit[i]){
+15                visit[i]=true;
+16                temp.add(nums[i]);
+17                per(nums,visit,list,temp);
+18                temp.remove(temp.size()-1);
+19                visit[i]=false;
+20            }
+21        }
+22    }
+23}
