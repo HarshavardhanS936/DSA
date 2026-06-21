1a269b1fd440947d435006b25798099664a8b1b6
HarshavardhanS936 <shv9306@gmail.com>
2026-06-21 17:43:47 +0530

    feat: Add Unknown Problem

--- General/Unknown Problem/java/Solution.java
+++ General/Unknown Problem/java/Solution.java
@@ -3,21 +3,32 @@
 3 * public class ListNode {
 4 *     int val;
 5 *     ListNode next;
-6 *     ListNode(int x) {
-7 *         val = x;
-8 *         next = null;
-9 *     }
-10 * }
-11 */
-12public class Solution {
-13    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-14        ListNode a=headA,b=headB;
-15        while(a != b){
-16            if(a==null) a=headB;
-17            else a=a.next;
-18            if(b==null)b=headA;
-19            else b=b.next;
-20        }
-21        return a;
-22    }
-23}
+6 *     ListNode() {}
+7 *     ListNode(int val) { this.val = val; }
+8 *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+9 * }
+10 */
+11class Solution {
+12    public ListNode removeNthFromEnd(ListNode head, int n) {
+13        ListNode dummy = new ListNode(0);
+14        dummy.next = head;
+15        ListNode fast = dummy;
+16        ListNode slow = dummy;
+17        for (int i = 0; i <= n; i++) {
+18            fast = fast.next;
+19        }
+20        while (fast != null) {
+21            fast = fast.next;
+22            slow = slow.next;
+23        }
+24        slow.next = slow.next.next;
+25        return dummy.next;
+26    }
+27    public static void printList(ListNode head) {
+28        while (head != null) {
+29            System.out.print(head.val + " ");
+30            head = head.next;
+31        }
+32        System.out.println();
+33    }
+34}
