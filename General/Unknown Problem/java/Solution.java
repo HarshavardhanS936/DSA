bb98ad248b8ce9d92c5df1a0152fa92d84ff949b
HarshavardhanS936 <shv9306@gmail.com>
2026-06-21 17:48:08 +0530

    feat: Add Unknown Problem

--- General/Unknown Problem/java/Solution.java
+++ General/Unknown Problem/java/Solution.java
@@ -1,19 +1,17 @@
 1class Solution {
-2    public List<List<Integer>> combinationSum(int[] nums, int target) {
+2    public List<List<Integer>> subsets(int[] nums) {
 3        List<List<Integer>> ans=new ArrayList<>();
-4        sub(0,nums,target,ans,new ArrayList<>());
+4        sub(0,nums,ans,new ArrayList<>());
 5        return ans;
 6    }
-7    public static void sub(int index,int nums[],int target,List<List<Integer>> ans,List<Integer> temp){
-8        if(target==0){
+7    public static void sub(int index,int nums[],List<List<Integer>> ans,List<Integer> temp){
+8        if(index==nums.length){
 9            ans.add(new ArrayList<>(temp));
 10            return;
 11        }
-12        if(target<0)return;
-13        for(int i=index;i<nums.length;i++){
-14            temp.add(nums[i]);
-15            sub(i,nums,target-nums[i],ans,temp);
-16            temp.remove(temp.size()-1);
-17        }
-18    }
-19}
+12        temp.add(nums[index]);
+13        sub(index+1,nums,ans,temp);
+14        temp.remove(temp.size()-1);
+15        sub(index+1,nums,ans,temp);
+16    }
+17}
