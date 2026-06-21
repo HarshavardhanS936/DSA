921378db8a26cd9f9b7acf87269f9eb537281ac4
HarshavardhanS936 <shv9306@gmail.com>
2026-06-21 13:37:53 +0530

    feat: Add Unknown Problem

--- General/Unknown Problem/java/Solution.java
+++ General/Unknown Problem/java/Solution.java
@@ -3,26 +3,12 @@
 3 * public class ListNode {
 4 *     int val;
 5 *     ListNode next;
-6 *     ListNode() {}
-7 *     ListNode(int val) { this.val = val; }
-8 *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-9 * }
-10 */
-11class Solution {
-12    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-13        ListNode dummy = new ListNode();
-14        ListNode cur = dummy;
-15        while (list1 != null && list2 != null) {
-16            if (list1.val > list2.val) {
-17                cur.next = list2;
-18                list2 = list2.next;
-19            } else {
-20                cur.next = list1;
-21                list1 = list1.next;
-22            }
-23            cur = cur.next;
-24        }
-25        cur.next = (list1 != null) ? list1 : list2;
-26        return dummy.next;        
-27    }
-28}
+6 *     ListNode(int x) { val = x; }
+7 * }
+8 */
+9class Solution {
+10    public void deleteNode(ListNode node) {
+11        node.val = node.next.val;
+12        node.next = node.next.next;
+13    }
+14}
