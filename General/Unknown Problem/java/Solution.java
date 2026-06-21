8617b16acdd0603646b9dc16c8a5d04f35476a18
HarshavardhanS936 <shv9306@gmail.com>
2026-06-21 13:37:51 +0530

    feat: Add Unknown Problem

--- General/Unknown Problem/java/Solution.java
+++ General/Unknown Problem/java/Solution.java
@@ -9,15 +9,20 @@
 9 * }
 10 */
 11class Solution {
-12    public ListNode reverseList(ListNode head) {
-13        ListNode pre=null;
-14        ListNode cur=head;
-15        while(cur!=null){
-16            ListNode temp=cur.next;
-17            cur.next=pre;
-18            pre=cur;
-19            cur=temp;
-20        }
-21        return pre;
-22    }
-23}
+12    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+13        ListNode dummy = new ListNode();
+14        ListNode cur = dummy;
+15        while (list1 != null && list2 != null) {
+16            if (list1.val > list2.val) {
+17                cur.next = list2;
+18                list2 = list2.next;
+19            } else {
+20                cur.next = list1;
+21                list1 = list1.next;
+22            }
+23            cur = cur.next;
+24        }
+25        cur.next = (list1 != null) ? list1 : list2;
+26        return dummy.next;        
+27    }
+28}
