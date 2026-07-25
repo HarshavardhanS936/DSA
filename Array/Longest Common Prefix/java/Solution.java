252d89f9eada80b7f1506b37bac3f85919b25a32
Hv <shv9306@gmail.com>
2026-07-25 10:59:58 +0530

    feat: Add Longest Common Prefix

--- Array/Longest Common Prefix/java/Solution.java
+++ Array/Longest Common Prefix/java/Solution.java
@@ -3,7 +3,7 @@ class Solution {
         String s1=strs[0];
         String s2=strs[strs.length-1];
         int ind=0;
-        while(ind<s1.length()&&ind<s2.length){
+        while(ind<s1.length()&&ind<s2.length()){
             if(s1.charAt(ind)==s2.charAt(ind))ind++;
         }
         return s1.substring(0,ind);
